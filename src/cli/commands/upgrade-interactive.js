06bc3cb71ae1658c2ca98a2e7b05557f558e8321
Carl Rosell <me@carlrosell.com>
2017-08-20 22:55:02 +0200

    Fix upgrade-interactive header columns.

--- src/cli/commands/upgrade-interactive.js
+++ src/cli/commands/upgrade-interactive.js
@@ -38,16 +38,19 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
   const maxLengthArr = {
     name: 'name'.length,
     current: 'from'.length,
-    range: 'range'.length,
+    range: 'latest'.length,
     [outdatedFieldName]: 'to'.length,
   };
 
-  if (flags.latest) {
-    maxLengthArr.range = 'latest'.length;
+  const keysWithDynamicLength = ['name', 'current', outdatedFieldName];
+
+  if (!flags.latest) {
+    maxLengthArr.range = 'range'.length;
+    keysWithDynamicLength.push('range');
   }
 
   deps.forEach(dep =>
-    ['name', 'current', 'range', outdatedFieldName].forEach(key => {
+    keysWithDynamicLength.forEach(key => {
       maxLengthArr[key] = Math.max(maxLengthArr[key], dep[key].length);
     }),
   );
